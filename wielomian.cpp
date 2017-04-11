85d637ab0146be01c7708db05cc2d8887b0539dc
darrineth <rak.rafa@gmail.com>
2017-04-11 10:18:12 +0200

    commit 6

---
+++ wielomian.cpp
@@ -0,0 +1,44 @@
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+
+int n;
+double x;
+const int M=200;
+double T[M];
+
+int czytaj()
+{
+    int i;
+
+    do
+    {
+        printf("Dla wielomianu a :\n");
+        printf("podaj stopien wielomianu. \nn=");
+        scanf("%d:\n",&n);
+        if(n<0)printf("stopien wielomianu musi byc wiekszy od 0 \n");
+        else if(n>M)printf("stopien wielomianu usi byc mniejszy od %d \n", M);
+    }
+    while (n<0||n>(M-1));
+    printf("podaj wartosc X. \nX=");
+    scanf("%lf", &x);
+    printf("podaj wartosc wspolczynnikow A: \n");
+    for(i=n;i>=0;i--)
+    {
+                     printf("A%d=", i);
+                     scanf("%lf", T+i);
+    }
+    printf("n=%d x=%.3lf\n",n,x);
+    return n;
+}
+int main()
+{
+    int i;
+    double S=0,N;
+    czytaj();
+    for(i=n;i>=0;i--) S+=(T[i]*(pow(x,i)));
+    printf("wartosc wielomianu wynosi: \ny(x)=%.4lf\n",S);
+    printf("X=%.3lf n=%d",x,n);
+    scanf("%d",&N);
+    return 0;
+}
