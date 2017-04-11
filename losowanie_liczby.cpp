8e19ff5f9dd1dc2f7dbd1839f27ae575cbdcfe2f
darrineth <rak.rafa@gmail.com>
2017-04-11 10:20:16 +0200

    commit 8

---
+++ losowanie_liczby.cpp
@@ -0,0 +1,53 @@
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    int tab[20];
+    srand(time(0));
+    cout<<"Wygenerowalem tablice liczb: \n";
+    for(int i=0;i<20;i++)
+
+    {
+    tab [i]=rand()%20;
+    cout<< tab[i]<<"\n\n";
+    }
+
+int suma=0;
+for(int i=0;i<20;i++)
+{
+suma+=tab[i];
+}
+
+cout<<"\nSUMA = "<<suma;
+int max=tab[0];
+int min=tab[0];
+int wiekszych=0;
+int mniejszych=0;
+for(int i=0;i<20;i++)
+{
+        if(tab[i]>max)
+        max=tab[i];
+        if(tab[i]<min)
+        min=tab[i];
+        if(tab[i]>=10)
+        wiekszych++;
+        if(tab[i]<10)
+        mniejszych++;
+        }
+
+        cout<<"\nMax: "<<max;
+        cout<<"\nmin: "<<min;
+        cout<<"\nwiekszych: "<<wiekszych;
+        cout<<"\nmniejszych: "<<mniejszych;
+        cout<<" \n\n";
+
+
+
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
